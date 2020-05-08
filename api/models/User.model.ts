5ce9d3a60e7f1f463d34c52e3213a94516f525df
Louis Bailleau <louis.bailleau@free.fr>
2020-05-08 17:19:32 +0200

    Created user model
    - Created user model
    - Created user roles typing

---
+++ api/models/User.model.ts
@@ -0,0 +1,74 @@
+import bcrypt from 'bcrypt'
+import {
+  Table,
+  Model,
+  IsUUID,
+  PrimaryKey,
+  Column,
+  Max,
+  Unique,
+  IsEmail,
+  AllowNull,
+  DataType,
+  Default,
+  BeforeUpdate,
+  BeforeCreate,
+} from 'sequelize-typescript'
+import { UserRole, IUserRole } from '~/types/UserRole'
+
+@Table({
+  timestamps: true,
+})
+export default class User extends Model<User> {
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  id!: string
+
+  @Max(100)
+  @Unique
+  @Column
+  username!: string
+
+  @Column
+  password!: string
+
+  @IsEmail
+  @AllowNull
+  @Column
+  email!: string
+
+  @Default(UserRole.user)
+  @Column(DataType.ENUM(...Object.values(UserRole)))
+  role!: IUserRole
+
+  @Default('en')
+  @Column
+  language!: string
+
+  @BeforeUpdate
+  @BeforeCreate
+  static async cryptPassword(instance: User) {
+    await cryptPassword(instance.password).then((success) => {
+      instance.password = success
+    })
+  }
+
+  validPassword(password: string) {
+    return bcrypt.compare(password, this.password)
+  }
+}
+
+function cryptPassword(password: string): Promise<string> {
+  return new Promise(function (resolve, reject) {
+    bcrypt.genSalt(10, (err, salt) => {
+      // Encrypt password using bcrypt module
+      if (err) return reject(err)
+
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) return reject(err)
+        return resolve(hash)
+      })
+    })
+  })
+}
