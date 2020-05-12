d37de98ac28810c08736af176dd93ba2b78db262
Louis Bailleau <louis.bailleau@free.fr>
2020-05-12 23:12:06 +0200

    Created modal component
    - Fixed Navbar z-index

---
+++ components/TModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="fixed top-0 left-0 z-20 w-full h-screen flex flex-row justify-center items-center bg-gray-900 bg-opacity-75 transition-all ease-out duration-300"
+  >
+    <div class="bg-white rounded shadow p-8 max-w-xl text-center flex-1">
+      <div class="mb-4 text-2xl font-bold">
+        <slot name="title"></slot>
+      </div>
+      <div class="mb-8 text-justify">
+        <slot name="description"></slot>
+      </div>
+      <div>
+        <slot name="actions"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class TModal extends Vue {}
+</script>
