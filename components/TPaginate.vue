8dabf00ba3634610119efb9e4b6a6ac090bc57f3
Louis Bailleau <louis.bailleau@free.fr>
2020-05-13 22:00:23 +0200

    Created pagination component

---
+++ components/TPaginate.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="flex text-gray-700">
+      <nuxt-link
+        v-if="$route.params.id > 1"
+        :to="changePage(current - 1)"
+        class="h-10 w-10 mr-1 flex justify-center items-center rounded-full hover:bg-gray-200 cursor-pointer"
+      >
+        <i class="text-xl fas fa-caret-left"></i>
+      </nuxt-link>
+
+      <nuxt-link
+        v-for="index in list"
+        :key="index"
+        :to="changePage(index + 1)"
+        class="h-10 w-10 mr-1 flex justify-center items-center rounded-full cursor-pointer"
+        :class="{
+          'bg-green-400 text-white': current === index + 1,
+          'hover:bg-gray-200': current !== index + 1,
+        }"
+      >
+        {{ index + 1 }}
+      </nuxt-link>
+
+      <nuxt-link
+        v-if="$route.params.id < last"
+        :to="changePage(current + 1)"
+        class="h-10 w-10 mr-1 flex justify-center items-center rounded-full hover:bg-gray-200 cursor-pointer"
+      >
+        <i class="text-xl fas fa-caret-right"></i>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { range } from 'lodash'
+
+@Component
+export default class TPagination extends Vue {
+  @Prop({ type: Number, required: true })
+  current!: number
+
+  @Prop({ type: Number, required: true })
+  last!: number
+
+  @Prop({ type: Function, required: true })
+  changePage!: Function
+
+  get list() {
+    if (this.last > 6) {
+    } else {
+      return range(this.last)
+    }
+  }
+}
+</script>
