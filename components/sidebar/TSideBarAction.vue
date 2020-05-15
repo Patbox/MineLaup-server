5d03f1fcdc6b9981154a637de4eb7cc2ae273ab2
Louis Bailleau <louis.bailleau@free.fr>
2020-05-15 18:45:18 +0200

    Updated sidebar components
    - Optimized sidebar button
    - Created sidebar action button

--- components/sidebar/TSideBarAction.vue
+++ components/sidebar/TSideBarAction.vue
@@ -4,16 +4,15 @@
     @mouseenter="showTitle = true"
     @mouseleave="showTitle = false"
   >
-    <nuxt-link
+    <a
       tag="span"
       class="text-white w-12 h-10 flex hover:text-green-400 cursor-pointer border-l-2 border-transparent"
-      :to="to"
-      :class="{ 'border-green-400 text-green-400': isCurrentRoute }"
+      @click="$emit('click')"
     >
       <span class="m-auto text-xl">
         <i class="fas" :class="'fa-' + icon"></i>
       </span>
-    </nuxt-link>
+    </a>
     <span
       v-if="showTitle"
       class="absolute bg-gray-800 px-1 rounded top-0 left-0 mt-2 ml-12 whitespace-no-wrap"
@@ -27,26 +26,16 @@
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
 
 @Component
-export default class TSideBarButton extends Vue {
+export default class TSideBarAction extends Vue {
   @Prop({ required: true })
   name!: string
 
   @Prop({ required: true })
   icon!: string
 
-  @Prop({ required: true })
-  to!: string
-
   @Prop()
   exact!: boolean
 
   showTitle: boolean = false
-
-  get isCurrentRoute() {
-    if (this.exact) {
-      return this.$route.path === this.to
-    }
-    return this.$route.path.startsWith(this.to)
-  }
 }
 </script>
