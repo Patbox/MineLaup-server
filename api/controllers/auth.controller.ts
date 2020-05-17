2b8494b49a2e88e8a4561ee997ff4481c9d2ef7d
Louis Bailleau <louis.bailleau@free.fr>
2020-05-17 22:29:34 +0200

    Created "update password" API
    - Created update password route
    - Created update password function in Auth controller

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -237,6 +237,114 @@ async function update(req: Request, res: Response) {
       }
 
       return res.status(403).json({
+        message: 'Bad password.',
+        errorKey: 'auth.login.bad_password',
+      })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).send(error)
+    }
+  })
+}
+
+async function updatePassword(req: Request, res: Response) {
+  const token = req.headers.authorization
+
+  if (!token) {
+    return res.status(400).send({
+      message: 'Authorization header not found.',
+      errorKey: 'auth.login.token_not_found',
+    })
+  }
+
+  const schema = Joi.object({
+    current: Joi.string().required(),
+    new: Joi.string().required(),
+    confirm: Joi.ref('new'),
+  })
+
+  const { error }: ValidationResult = schema.validate(req.body, {
+    abortEarly: false,
+  })
+
+  if (error) {
+    const formatedErrors: any = {}
+
+    for (const e of error.details) {
+      formatedErrors[e.path[0]] = {
+        message: e.message,
+        errorKey: `error.validation.${e.type.replace(/\./g, '_')}`,
+      }
+    }
+
+    return res.status(400).send({
+      errors: formatedErrors,
+    })
+  }
+
+  await jwt.verify(token, config.get().jwt_token, async function (
+    error:
+      | jwt.JsonWebTokenError
+      | jwt.NotBeforeError
+      | jwt.TokenExpiredError
+      | null,
+    result: object | undefined
+  ) {
+    if (error) {
+      return res.status(403).json({
+        message: 'Provided token is invalid.',
+        errorKey: 'auth.login.invalid_token',
+        error,
+      })
+    }
+
+    try {
+      // @ts-ignore
+      const user = await Users.findOne({ where: { id: result.id } })
+
+      if (user && (await user.validPassword(req.body.current))) {
+        await user.update({
+          password: req.body.new,
+        })
+
+        const userNew = await Users.findOne({
+          // @ts-ignore
+          where: { id: result.id },
+          raw: true,
+        })
+
+        if (userNew) {
+          const token = jwt.sign(
+            {
+              id: userNew.id,
+              username: userNew.username,
+              email: userNew.email,
+              role: userNew.role,
+              language: userNew.language,
+              colorMode: userNew.colorMode,
+            },
+            config.get().jwt_token,
+            {
+              expiresIn: '1d',
+            }
+          )
+
+          return res.status(200).json({
+            message: 'User updated.',
+            user: {
+              id: userNew.id,
+              username: userNew.username,
+              email: userNew.email,
+              role: userNew.role,
+              language: userNew.language,
+              colorMode: userNew.colorMode,
+            },
+            token,
+          })
+        }
+      }
+
+      return res.status(403).json({
         message: 'Provided token is invalid.',
         errorKey: 'auth.login.invalid_token',
       })
@@ -251,4 +359,5 @@ export default {
   login,
   user,
   update,
+  updatePassword,
 }
--- api/routers/auth.router.ts
+++ api/routers/auth.router.ts
@@ -6,5 +6,6 @@ const router = Router()
 router.post('/login', AuthController.login)
 router.get('/user', AuthController.user)
 router.post('/update', AuthController.update)
+router.post('/change-password', AuthController.updatePassword)
 
 export default router
