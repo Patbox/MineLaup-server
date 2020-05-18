27bf07b00b0f4bb1c4db9cefe2e3cc5ebd6f85c2
Louis Bailleau <louis.bailleau@free.fr>
2020-05-18 08:20:58 +0200

    Created `is-logged` middleware and updated all functions in controllers

--- api/routers/auth.router.ts
+++ api/routers/auth.router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
 import AuthController from '../controllers/auth.controller'
+import isLoggedMiddleware from '../middleware/is-logged.middleware'
 
 const router = Router()
 
 router.post('/login', AuthController.login)
-router.get('/user', AuthController.user)
-router.post('/update', AuthController.update)
-router.post('/change-password', AuthController.updatePassword)
+router.get('/user', isLoggedMiddleware, AuthController.user)
+router.post('/update', isLoggedMiddleware, AuthController.update)
+router.post(
+  '/change-password',
+  isLoggedMiddleware,
+  AuthController.updatePassword
+)
 
 export default router
