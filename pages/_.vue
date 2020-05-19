357b4c3edb376d39127c9b777e5a2cffd37e05af
Louis Bailleau <louis.bailleau@free.fr>
2020-05-19 11:00:16 +0200

    Updated 404 page

--- pages/_.vue
+++ pages/_.vue
@@ -5,8 +5,14 @@
         <i class="fas fa-map-signs"></i>
       </span>
       <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-400">
-        {{ $t('pages.not_found') }}
+        {{ $t('pages.not_found.title') }}
       </h1>
+      <a
+        class="cursor-pointer hover:text-green-400 transition-colors ease-out duration-150"
+        @click="$router.back()"
+      >
+        {{ $t('pages.not_found.link') }}
+      </a>
     </div>
   </div>
 </template>
