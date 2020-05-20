a0b000fbb4ed2d8d95f661df3c8639f476bc445b
Louis Bailleau <louis.bailleau@free.fr>
2020-05-20 10:47:30 +0200

    Created new version and fetch version API
    - Created function new and fetch version in modpack controller
    - Created route for new and fetch version in modpack router

--- api/controllers/modpack.controller.ts
+++ api/controllers/modpack.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import Joi from '@hapi/joi'
 import Modpack from '../models/Modpack.model'
+import ModpackVersion from '../models/ModpackVersion.model'
 
 async function create(req: Request, res: Response) {
   const schema = Joi.object({
@@ -94,12 +95,111 @@ async function get(req: Request, res: Response) {
         id: req.query.id,
         ownerId: req.body.user.id,
       },
+      raw: true,
     })
 
     if (modpack) {
-      return res.status(200).send({
-        modpack,
+      const versions = await ModpackVersion.findAll({
+        where: {
+          modpackId: modpack.id,
+        },
+        raw: true,
       })
+
+      return res.status(200).send({
+        modpack: {
+          ...modpack,
+          versions,
+        },
+      })
+    }
+
+    return res.sendStatus(404)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    return res.status(500).send(error)
+  }
+}
+
+async function newVersion(req: Request, res: Response) {
+  const { error } = Joi.object({
+    name: Joi.string().required(),
+    version: Joi.string().pattern(/^(\d+\.)?(\d+\.)?(\*|\d+)$/, {
+      name: 'Semver',
+    }),
+    modpackId: Joi.string().required(),
+    user: Joi.any(),
+  }).validate(req.body)
+
+  if (error) {
+    const formatedErrors: any = {}
+
+    for (const e of error.details) {
+      formatedErrors[e.path[0]] = {
+        message: e.message,
+        errorKey: `error.validation.${e.type.replace(/\./g, '_')}`,
+      }
+    }
+
+    return res.status(400).send({
+      errors: formatedErrors,
+    })
+  }
+
+  try {
+    await ModpackVersion.create(
+      {
+        name: req.body.name,
+        modpackId: req.body.modpackId,
+        version: req.body.version,
+      },
+      {
+        raw: true,
+      }
+    )
+
+    return res.sendStatus(200)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    return res.status(500).send(error)
+  }
+}
+
+async function versions(req: Request, res: Response) {
+  const { error } = Joi.object({
+    id: Joi.string().required(),
+  }).validate(req.query)
+
+  if (error) {
+    return res.status(400).send({
+      message: 'Missing id parameters in the request',
+      error: 'error.validation.missing_request_params',
+    })
+  }
+
+  try {
+    const modpack = await Modpack.findOne({
+      where: {
+        id: req.query.id,
+        ownerId: req.body.user.id,
+      },
+    })
+
+    if (modpack) {
+      const versions = await ModpackVersion.findAll({
+        where: {
+          modpackId: modpack.id,
+        },
+        order: [['createdAt', 'DESC']],
+      })
+
+      if (versions) {
+        return res.status(200).send({
+          versions,
+        })
+      }
     }
 
     return res.sendStatus(404)
@@ -114,4 +214,6 @@ export default {
   create,
   list,
   get,
+  newVersion,
+  versions,
 }
--- api/routers/modpack.router.ts
+++ api/routers/modpack.router.ts
@@ -7,5 +7,7 @@ const router = Router()
 router.post('/create', isLoggedMiddleware, ModpackController.create)
 router.get('/list', isLoggedMiddleware, ModpackController.list)
 router.get('/', isLoggedMiddleware, ModpackController.get)
+router.post('/versions/new', isLoggedMiddleware, ModpackController.newVersion)
+router.get('/versions', isLoggedMiddleware, ModpackController.versions)
 
 export default router
